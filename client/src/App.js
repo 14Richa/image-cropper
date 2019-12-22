2ba622efb2ada3622031e0fca45d59f799344fd3
Richa Sharma <onlinericha19@gmail.com>
2019-12-22 20:10:06 +0530

    added ant library for ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,140 +2,242 @@ import React from 'react';
 import Dropzone from 'react-dropzone';
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
-import {image64toCanvasRef, extractImageFileExtensionFromBase64, downloadBase64File, base64StringtoFile} from './ResuableUtils';
+import {image64toCanvasRef, extractImageFileExtensionFromBase64, downloadBase64File, base64StringtoFile, makeClientCrop} from './ResuableUtils';
+import { Upload, message, Icon, PageHeader, Tabs, Button, Statistic, Descriptions } from 'antd';
+import { Menu, Dropdown, Row, Col } from 'antd';
+import 'antd/dist/antd.css'
+//const imgMaxSize = 100000000//bytes;
+//const FileTypes = ['image/x-png', 'image/png', 'image/jpg', 'image/jpeg', 'image/gif']
 
 
-const imgMaxSize = 100000000//bytes;
-const FileTypes = ['image/x-png', 'image/png', 'image/jpg', 'image/jpeg', 'image/gif']
+
 class App extends React.Component {
-  //const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
-  constructor(props) {
+
+constructor(props) {
     super(props)
-    this.imagePreviewCanvasRef = React.createRef()
+    this.imagePreviewCanvasRef = React.createRef();
     this.state = {
-        imgSrc: null, 
+        imgSrc: null,
+        croppedImageURL: null,
+        fileList: [], 
         crop: {
-            aspects: 1/1,
+            unit: 'px',
+            height: 755,
+            width: 450,
+            zoom: 1,
             
         } 
     }
-  }
-  
-
-    alertFile = (files) => {
-        if(files && files.length>0){
-            const currentFile = files[0];
-            const currentFileSize = currentFile.size;
-            const currentFileType = currentFile.type;
-            if(currentFileSize > imgMaxSize){
-                alert("This file is too big");
-                return false;
-            }
-            if(!FileTypes.includes(currentFileType)){
-                alert("This file type is not supported");
-                return false;
-            }
-            return true;
-        }
-
-    };
-
-    onDropFn = (files, rejectedFiles) => {
-        console.log(files);
-        if(rejectedFiles && rejectedFiles.length > 0){
-           this.alertFile(rejectedFiles);
-        }
-        if(files && files.length>0 ){
-            const isVerified = this.alertFile(files)
-            if(isVerified) {
-                const currentFile = files[0]
-                const reader = new FileReader()
-                reader.addEventListener("load", () => {
-                    this.setState({
-                        imgSrc: reader.result
-                    })
-
-                }, false)
-
-                reader.readAsDataURL(currentFile)
-            }
-
-
-        }
-    }
+}
     
 
-    handleImageLoaded = (image) => {
-        console.log(image)
+onDropFn = (info) => {
+    let fileList = [];
+    console.log(info);
+    if (info.file.status === 'uploading' ) {
+
+        //Reduce the file to the latest one.
+        fileList = [...info.fileList];
+        fileList = fileList.slice(-1);
+        const currentFile = fileList[0].originFileObj;
+        const reader = new FileReader()
+        
+        reader.addEventListener("load", () => {
+            let image = new Image();
+            image.src = reader.result;
+            image.onload = () => {
+                if(image.width !== 1024 || image.height !== 1024){
+                    message.error(`Image is ${image.height} x ${image.width}. Please upload 1024 x 1024 image`);
+                    fileList = [];
+                }
+                else
+                {
+                    message.success(`${info.file.name} file uploaded successfully`);
+
+                    fileList = fileList.map(file => {
+                                if (file.response) {
+                                    file.url = file.response.url;
+                                  }
+                                  return file;
+                                });
+                    
+                    this.setState({
+                    imgSrc: reader.result,
+                    imgSrcExt:  extractImageFileExtensionFromBase64(reader.result),
+                    fileList: fileList 
+                    })
+                }
+            };
+
+        }, false)
+        reader.readAsDataURL(currentFile);  
     }
-    handleOnCropChange = (crop) => {
-        //console.log(crop)
+    else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+        this.setState({fileList: []});
+
+    }
+};
+
+
+
+OnImageLoad = (image) => {
+        //console.log(image)
+        this.imageRef = image;
+};
+OnCropChange = (crop) => {
+        console.log("Crop inside Cropchange ", crop)
         this.setState({crop:crop})
-        console.log(this.state)
-    }
+        //console.log(this.state)
+};
     handleOnCropComplete = (crop, pixelCrop) => {
-        //console.log(crop,pixelCrop)
 
-        const canvasRef = this.imagePreviewCanvasRef.current
-        const imgSrc = this.state
-        image64toCanvasRef(canvasRef, imgSrc, pixelCrop)
+        console.log("Crop inside Cropcomplete", crop);
+        
+        const canvasRef = this.imagePreviewCanvasRef.current;
+        //console.log(canvasRef);
+        const {imgSrc} = this.state;
+        console.log("CanvasRef", canvasRef);
+        if(this.imageRef) image64toCanvasRef(canvasRef, imgSrc, crop, this.imageRef);
+    };
+
+    handleClearToDefault = (event) =>{
+        //if (event) event.preventDefault()
+        const canvas = this.imagePreviewCanvasRef.current
+        const ctx = canvas.getContext('2d');
+        console.log(canvas);
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+        this.setState({
+            imgSrc: null,
+            imgSrcExt: null
+        })
+    };
+
+    changeCrop = ({ key }) => {
+    //message.info(`Click on item ${key}`);
+    let h,w;
+    switch(key) {
+        case "1":
+        h = 755; w= 450;
+        break;
+        case "2":
+        h = 365; w= 450;
+        break;
+        case "3":
+        h = 365; w= 212;
+        break;
+        case "4":
+        h = 380; w= 380;
+        break;
+        default:
+        h=0; w=0;
+    }
+    this.setState({
+        crop: {
+            x:0,
+            y:0,
+            unit: "px",
+            height: h,
+            width: w
+        }
+    })
+    this.handleOnCropComplete({
+            unit: "px",
+            height: h,
+            width: w
+        });
+
     }
 
-    handleOnDownloadClick = (event) => {
+    menu = (
+    <Menu onClick={this.changeCrop}>
+    <Menu.Item key="1">755 x 450</Menu.Item>
+    <Menu.Item key="2">365 x 450</Menu.Item>
+    <Menu.Item key="3">365 x 212 </Menu.Item>
+    <Menu.Item key="4">380 x 380 </Menu.Item>
+    <Menu.Item key="5">Custom</Menu.Item>
+    </Menu>
+    );
+
+    handleDownloadClick = (event) => {
         event.preventDefault()
-        const canvasRef = this.imagePreviewCanvasRef.current
-        const imgSrc = this.state.imgSrc;
-        const fileExtension = extractImageFileExtensionFromBase64(imgSrc)
-        const myFilename = "previewFile." + fileExtension
+        const {imgSrc}  = this.state
+        if (imgSrc) {
+            const canvasRef = this.imagePreviewCanvasRef.current        
+            const {imgSrcExt} =  this.state
+            const imageData64 = canvasRef.toDataURL('image/' + imgSrcExt)
+            const myFilename = "previewFile." + imgSrcExt
 
-        const myNewCropperFile = base64StringtoFile(imgSrc)
-        console.log(myNewCropperFile)
-
-        // to download file 
-
-        downloadBase64File(imgSrc, myFilename)
+            // file to be uploaded
+            const myNewCroppedFile = base64StringtoFile(imageData64, myFilename)
+            console.log(myNewCroppedFile)
+            // download file
+            downloadBase64File(imageData64, myFilename)
+            this.handleClearToDefault()
+        }
     }
 
 render(){
-    const {imgSrc} = this.state
-      return (
+    const {imgSrc, croppedImageURL, fileList} = this.state
+      return(
         <div>
-        
+            <PageHeader style={{border: '1px solid rgb(235, 237, 240)'}}
+                        title="Image Cropper"
+                        subTitle="Small utility to crop images and store them locally"
+            >
 
-        {imgSrc !== null ? 
-            <div> 
-                
+            <Descriptions size="small" column={2}>
+                <Descriptions.Item label="Created">Richa Sharma</Descriptions.Item>
+                <Descriptions.Item label="Tech"> <a href="https://reactjs.org/" target = "_blank">ReactJS</a> and <a href="https://ant.design/docs/react/introduce" target = "_blank">Ant design </a></Descriptions.Item>
+                <Descriptions.Item label="Remarks">
+                    Upload an image and choose the size you wish to crop to. You can choose pre-determined blocks or can do custom crop as well. It accepts images of size 1024x1024 only. 
+                </Descriptions.Item>
+            </Descriptions>
 
-           
-            <ReactCrop src = {imgSrc} 
-            crop = {this.state.crop} 
-            onImageLoaded = {this.handleImageLoaded}
-            onComplete = {this.handleOnCropComplete}
-            onChange = {this.handleOnCropChange}/>
-           
+            <Row style={{ padding: '20px 50px' }}>
+                <Col span={4}>
+                    <Upload  fileList={[]} onChange={this.onDropFn}  >
+                    <Button  disabled={imgSrc}>
+                        <Icon type="upload" /> Click to Upload
+                    </Button>
+                    </Upload>  
+                </Col>
+                <Col span={8}>
+                    <Dropdown.Button overlay={this.menu} disabled={!imgSrc}>
+                        <a className="ant-dropdown-link" href="#">Select the crop size</a>
+                    </Dropdown.Button>
+                </Col>
+                <Col span={2}>
+                    <Button  disabled={!imgSrc} onClick={this.handleDownloadClick}>Download</Button>
+                </Col>
+                <Col span={6}>
+                    <Button   disabled={!imgSrc} onClick={this.handleClearToDefault}>Clear</Button>
+                </Col>
+            </Row>
+            </PageHeader>
 
-            <br />
-            <p> Preview Canvas Crop </p>
-            <canvas ref = {this.imagePreviewCanvasRef}></canvas>
-            <button onClick = {this.handleOnDownloadClick}> Download </button>
+        {
+            imgSrc !== null ? 
+            <div>
+                <Row style={{ padding: '20px 20px' }}> 
+                    <Col span={12}>
+                        <ReactCrop  src = {imgSrc} 
+                                    crop = {this.state.crop} 
+                                    onImageLoaded = {this.OnImageLoad}
+                                    onComplete = {this.handleOnCropComplete}
+                                    onChange = {this.OnCropChange}/>
+                    </Col>
+                    <Col span={4}>
+                    </Col>
+                    <Col span={8}>
+                    <p> Preview Crop </p>
+                        <canvas ref={this.imagePreviewCanvasRef}></canvas>
+                    </Col>
+                </Row>
             </div>
-
-           
-
-            : <Dropzone  onDrop={this.onDropFn} accept={FileTypes} multiple={false} maxSize={imgMaxSize}>
-            {({getRootProps, getInputProps}) => (
-                <section>
-                  <div {...getRootProps()}>
-                    <input {...getInputProps()} />
-                    <button style = {{marginTop: "140px", marginLeft: "740px"}} ><p> Click here to select a file</p></button>
-                  </div>
-                </section>
-            )}
-        </Dropzone>
-
-    }
-
-       
+            : ""
+        }        
         </div>
         
       );
@@ -143,4 +245,3 @@ render(){
 }
 
 export default App;
-
--- client/src/ResuableUtils.js
+++ client/src/ResuableUtils.js
@@ -26,29 +26,39 @@ export function downloadBase64File (base64Data, filename) {
 
 // Extract an Base64 Image's File Extension
 export function extractImageFileExtensionFromBase64 (base64Data) {
-  console.log(base64Data);
   return base64Data.substring('data:image/'.length, base64Data.indexOf(";base64"))
 }
 
+
+
 // Base64 Image to Canvas with a Crop
-export function image64toCanvasRef (canvasRef, image64, pixelCrop) {
+export function image64toCanvasRef (canvasRef, image64, pixelCrop, imageRef) {
   const canvas = canvasRef // document.createElement('canvas');
+  const scaleX = imageRef.naturalWidth / imageRef.width;
+  const scaleY = imageRef.naturalHeight / imageRef.height;
+  console.log(scaleX, "scaleX");
+  console.log(image64);
+  console.log(imageRef, "ImageRef");
   canvas.width = pixelCrop.width
   canvas.height = pixelCrop.height
   const ctx = canvas.getContext('2d')
   const image = new Image()
   image.src = image64
+  console.log(image);
   image.onload = function () {
-    ctx.drawImage(
-      image,
-      pixelCrop.x,
-      pixelCrop.y,
-      pixelCrop.width,
-      pixelCrop.height,
-      0,
-      0,
-      pixelCrop.width,
-      pixelCrop.height
-    )
+    if(canvas.width!=0)
+    {
+      ctx.drawImage(
+          image,
+          pixelCrop.x * scaleX ,
+          pixelCrop.y * scaleY,
+          pixelCrop.width * scaleX,
+          pixelCrop.height * scaleY,
+          0,
+          0,
+          pixelCrop.width,
+          pixelCrop.height
+        )
+    }
   }
 }
